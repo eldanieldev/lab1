b9262810ed7296520799cd7f3484f1d0e153259f
Daniel Hernandez <eldanieldev@gmail.com>
2017-10-03 22:50:34 +0000

    deployment version

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
+  get 'my_pages/home'
+
+  get 'my_pages/help'
+
+  get 'my_pages/about'
+
   resources :microposts
   resources :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
-  #'application#hello'
+  
+  #root 'usersrails s -b $IP -p $PORT#index'
+  
+  #root 'application#hello'
+  
+  root 'my_pages#home'
   
   
 end
